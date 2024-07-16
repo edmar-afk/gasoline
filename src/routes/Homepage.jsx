635b09eca8cc2902f4d884aedc1e246865395f02
project2479 <jaywrsnp6@gmail.com>
2024-07-16 13:39:36 +0800

    new

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,9 +1,9 @@
-/* eslint-disable react/no-unescaped-entities */ import logo from "../assets/img/logo.png";
+/* eslint-disable react/no-unescaped-entities */import { Link } from "react-router-dom";import logo from "../assets/img/logo.png";
 import { motion } from "framer-motion";
 function Homepage() {
 	return (
 		<>
-			<div className="bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100">
+			<div className="bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100 h-screen">
 				<div className="flex justify-center pt-12">
 					<motion.img
 						initial={{ scale: 0 }}
@@ -35,27 +35,28 @@ function Homepage() {
 							Stay updated on the latest fuel prices at various stations from San Miguel to Guipos to help you make
 							informed decisions and save money on your next fill-up.
 						</motion.p>
-
-						<motion.a
+						<motion.div
 							initial={{ opacity: 0, y: 0 }}
 							animate={{ opacity: 1, y: 20 }}
-							transition={{ duration: 1 }}
-							href="#"
-							className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full"
-							role="alert">
-							<span className="text-xs bg-purple-800 rounded-full px-4 py-1.5 mr-3">Go</span>{" "}
-							<span className="text-sm font-medium">Check the latest prices today</span>
-							<svg
-								className="ml-2 w-5 h-5"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg">
-								<path
-									fillRule="evenodd"
-									d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-									clipRule="evenodd"></path>
-							</svg>
-						</motion.a>
+							transition={{ duration: 1 }}>
+							<Link
+								to={"/"}
+								className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full"
+								role="alert">
+								<span className="text-xs bg-purple-800 rounded-full px-4 py-1.5 mr-3">Go</span>{" "}
+								<span className="text-sm font-medium">Check the latest prices today</span>
+								<svg
+									className="ml-2 w-5 h-5"
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg">
+									<path
+										fillRule="evenodd"
+										d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+										clipRule="evenodd"></path>
+								</svg>
+							</Link>
+						</motion.div>
 					</div>
 				</section>
 			</div>
