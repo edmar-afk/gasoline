6a104cae5ce34c9fafdb18267e26864622ff2046
project2479 <jaywrsnp6@gmail.com>
2024-07-30 17:33:12 +0800

    new face

--- src/components/owner/Pricing.jsx
+++ src/components/owner/Pricing.jsx
@@ -4,13 +4,13 @@ function Pricing() {
 		<>
 			<div className="flex flex-row items-center p-6">
 				<div className="flex flex-col items-center mr-4">
-					<p className="bg-red-500 text-white p-2 rounded-full px-[13px] text-3xl dark:shadow-red-100 shadow-xl dark:shadow-2xl">
+					<p className="bg-red-500 text-white p-2 rounded-full px-[13px] text-2xl shadow-xl">
 						<DiscountIcon className="mb-1" />
 					</p>
 					<p className="text-xs font-bold text-red-400">Promos</p>
 				</div>
 				<div className="flex flex-col items-center ml-4">
-					<p className="bg-blue-800 text-white p-2 rounded-full px-[17px] text-3xl dark:shadow-blue-100 shadow-xl dark:shadow-2xl">
+					<p className="bg-blue-800 text-white p-2 rounded-full px-[17px] text-2xl shadow-xl">
 						₱
 					</p>
 					<p className="text-xs font-bold text-blue-800 dark:text-blue-400">Pricing</p>
